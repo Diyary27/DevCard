5162f2538232d962a5b93ded6b04e1b50e544ae4
Diyary27 <naozary.diyar@gmail.com>
2021-12-21 02:29:03 +0330

    created models and made the project part dynamic

---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+
+        public Project(int id, string name, string description, string client)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Client = client;
+        }
+    }
+}
---
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>() {
+                new Project(1, "طراحی سایت", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.", "مشنری"),
+                new Project(2, "سایت فروشگاهی", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.", "مشنری"),
+                new Project(3, "پروژه مدارس", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.", "مشنری"),
+                new Project(4, "برنامه نویسی", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.", "مشنری"),
+
+        };
+
+            return View("_Projects", projects);
+    }
+}
+}
