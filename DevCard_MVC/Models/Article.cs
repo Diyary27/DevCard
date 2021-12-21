9e80a4a8a4c4d2accda009d4b4e05e96686bbe67
Diyary27 <naozary.diyar@gmail.com>
2021-12-21 14:14:04 +0330

    Made latest articles dynamic

--- DevCard_MVC/Models/Article.cs
+++ DevCard_MVC/Models/Article.cs
@@ -5,5 +5,12 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
+        public Article(int id, string title, string description)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+        }
     }
 }
---
+++ DevCard_MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>()
+            {
+                new Article(1,"لورم ایپسوم", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است."),
+                new Article(2,"لورم ایپسوم", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است."),
+                new Article(3,"لورم ایپسوم", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است."),
+                new Article(4,"لورم ایپسوم", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است."),
+                new Article(5,"لورم ایپسوم", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است."),
+                new Article(6,"لورم ایپسوم", "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است."),
+            };
+
+            return View("_LatestArticles", articles);
+        }
+    }
+}
